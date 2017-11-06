ff055384664ed969a91f16f8dae51f8cae8b12c6
Philippe Rivière <fil@rezo.net>
2017-11-06 12:08:29 +0100

    test delta()

--- test/versor-test.js
+++ test/versor-test.js
@@ -24,3 +24,9 @@ tape("90° versors", function(test) {
   test.ok(distance2(versor([ 0, 0, -90 ]), [ Math.sqrt(2)/2, 0, 0, -Math.sqrt(2)/2 ]) < 1e-8);
   test.end();
 });
+
+tape("delta", function(test) {
+  var v0 = versor([ 0, 0, 0 ]), v1 = versor([ 90, 0, 0 ]);
+  test.ok(distance2(versor.delta(v0,v1), [ 0.92388, 0.38268, 0, 0 ]) < 1e-8);
+  test.end();
+});
