8eefb684275c08b774a4da2804594afc7fca010c
Chris Henrick <henrick@google.com>
2018-10-12 14:23:50 -0700

    fixed some definitions && formated code

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
-// Type definitions for Versor 0.0.3
+// Type definitions for Versor 0.0.4
 // Project: https://github.com/Fil/versor
 // Definitions by: Chris Henrick <https://github.com/clhenrick>
+// Typescript Version: 3.0.3
 
 export as namespace versor;
 
@@ -9,15 +10,30 @@ export = versor;
 declare function versor(e: [number, number, number]): [number, number, number];
 
 declare namespace versor {
-    export function cartesian(e: [number, number]): [number, number, number];
-    
-    export function rotation(e: [number, number, number]): [number, number, number];
+  export function cartesian(e: [number, number]): [number, number, number];
 
-    export function delta(v0: number, v1: number, alpha?: number): [number, number, number, number];
+  export function rotation(
+    e: [number, number, number, number]
+  ): [number, number, number];
 
-    export function multiply(q0: number, q1: number): [number, number, number, number];
+  export function delta(
+    v0: [number, number, number],
+    v1: [number, number, number],
+    alpha?: number
+  ): [number, number, number, number];
 
-    function cross(v0: number, v1: number): [number, number, number];
+  export function multiply(
+    q0: [number, number, number, number],
+    q1: [number, number, number, number]
+  ): [number, number, number, number];
 
-    function dot(v0: number, v1: number): number;
+  function cross(
+    v0: [number, number, number],
+    v1: [number, number, number]
+  ): [number, number, number];
+
+  function dot(
+    v0: [number, number, number],
+    v1: [number, number, number]
+  ): number;
 }
