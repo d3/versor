93847b63f56d4eb7c6b3b0706170ece186d40b04
Philippe Rivière <fil@rezo.net>
2019-03-11 09:03:24 +0100

    versor.interpolate
    Use @mbostock's new implementation from https://observablehq.com/@d3/world-tour
    Add Versor.multiply, Versor.dot
    Keep the old API for the moment
    TBD: versor.delta()
    Ref: https://github.com/Fil/versor/issues/4

--- src/versor.js
+++ src/versor.js
@@ -2,6 +2,7 @@ var acos = Math.acos,
     asin = Math.asin,
     atan2 = Math.atan2,
     cos = Math.cos,
+    hypot = Math.hypot,
     max = Math.max,
     min = Math.min,
     PI = Math.PI,
@@ -10,18 +11,88 @@ var acos = Math.acos,
     radians = PI / 180,
     degrees = 180 / PI;
 
-// Returns the unit quaternion for the given Euler rotation angles [λ, φ, γ].
-var versor = function(e) {
-  var l = e[0] / 2 * radians, sl = sin(l), cl = cos(l), // λ / 2
-      p = e[1] / 2 * radians, sp = sin(p), cp = cos(p), // φ / 2
-      g = e[2] / 2 * radians, sg = sin(g), cg = cos(g); // γ / 2
-  return [
-    cl * cp * cg + sl * sp * sg,
-    sl * cp * cg - cl * sp * sg,
-    cl * sp * cg + sl * cp * sg,
-    cl * cp * sg - sl * sp * cg
-  ];
+class Versor {
+  static fromAngles([l, p, g]) {
+    l *= radians / 2;
+    p *= radians / 2;
+    g = (g||0) * radians / 2;
+    const sl = sin(l), cl = cos(l);
+    const sp = sin(p), cp = cos(p);
+    const sg = sin(g), cg = cos(g);
+    return [
+      cl * cp * cg + sl * sp * sg,
+      sl * cp * cg - cl * sp * sg,
+      cl * sp * cg + sl * cp * sg,
+      cl * cp * sg - sl * sp * cg
+    ];
+  }
+  static toAngles([a, b, c, d]) {
+    return [
+      atan2(2 * (a * b + c * d), 1 - 2 * (b * b + c * c)) * 180 / PI,
+      asin(max(-1, min(1, 2 * (a * c - d * b)))) * 180 / PI,
+      atan2(2 * (a * d + b * c), 1 - 2 * (c * c + d * d)) * 180 / PI
+    ];
+  }
+  static interpolateAngles(a, b) {
+    const i = Versor.interpolate(Versor.fromAngles(a), Versor.fromAngles(b));
+    return t => Versor.toAngles(i(t));
+  }
+  static interpolateLinear([a1, b1, c1, d1], [a2, b2, c2, d2]) {
+    a2 -= a1, b2 -= b1, c2 -= c1, d2 -= d1;
+    const x = new Array(4);
+    return t => {
+      const l = hypot(x[0] = a1 + a2 * t, x[1] = b1 + b2 * t, x[2] = c1 + c2 * t, x[3] = d1 + d2 * t);
+      x[0] /= l, x[1] /= l, x[2] /= l, x[3] /= l;
+      return x;
+    };
+  }
+  static interpolate([a1, b1, c1, d1], [a2, b2, c2, d2]) {
+    let dot = Versor.dot([a1, b1, c1, d1], [a2, b2, c2, d2]);
+    if (dot < 0) a2 = -a2, b2 = -b2, c2 = -c2, d2 = -d2, dot = -dot;
+    if (dot > 0.9995) return Versor.interpolateLinear([a1, b1, c1, d1], [a2, b2, c2, d2]); 
+    const theta0 = acos(max(-1, min(1, dot)));
+    const x = new Array(4);
+    const l = hypot(a2 -= a1 * dot, b2 -= b1 * dot, c2 -= c1 * dot, d2 -= d1 * dot);
+    a2 /= l, b2 /= l, c2 /= l, d2 /= l;
+    return t => {
+      const theta = theta0 * t;
+      const s = sin(theta);
+      const c = cos(theta);
+      x[0] = a1 * c + a2 * s;
+      x[1] = b1 * c + b2 * s;
+      x[2] = c1 * c + c2 * s;
+      x[3] = d1 * c + d2 * s;
+      return x;
+    };
+  }
+  static dot([a1, b1, c1, d1], [a2, b2, c2, d2]) {
+    return a1 * a2 + b1 * b2 + c1 * c2 + d1 * d2;
+  }
+  static multiply([a1, b1, c1, d1], [a2, b2, c2, d2]) {
+    return [
+      a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2,
+      a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2,
+      a1 * c2 - b1 * d2 + c1 * a2 + d1 * b2,
+      a1 * d2 + b1 * c2 - c1 * b2 + d1 * a2
+    ];
+  }
 }
+
+/*
+ * Old API
+ */
+
+// Returns the unit quaternion for the given Euler rotation angles [λ, φ, γ].
+var versor = Versor.fromAngles;
+
+// Returns the quaternion that represents q0 * q1.
+versor.multiply = Versor.multiply;
+
+// Returns the Euler rotation angles [λ, φ, γ] for the given quaternion.
+versor.rotation = Versor.toAngles;
+
+// interpolate angles
+versor.interpolate = Versor.interpolateAngles;
 
 // Returns Cartesian coordinates [x, y, z] given spherical coordinates [λ, φ].
 versor.cartesian = function(e) {
@@ -29,45 +100,24 @@ versor.cartesian = function(e) {
   return [cp * cos(l), cp * sin(l), sin(p)];
 };
 
-// Returns the Euler rotation angles [λ, φ, γ] for the given quaternion.
-versor.rotation = function(q) {
-  return [
-    atan2(2 * (q[0] * q[1] + q[2] * q[3]), 1 - 2 * (q[1] * q[1] + q[2] * q[2])) * degrees,
-    asin(max(-1, min(1, 2 * (q[0] * q[2] - q[3] * q[1])))) * degrees,
-    atan2(2 * (q[0] * q[3] + q[1] * q[2]), 1 - 2 * (q[2] * q[2] + q[3] * q[3])) * degrees
-  ];
-};
-
 // Returns the quaternion to rotate between two cartesian points on the sphere.
 // alpha for tweening [0,1]
 versor.delta = function(v0, v1, alpha) {
   if (arguments.length == 2) alpha = 1;
-  var w = cross(v0, v1), l = sqrt(dot(w, w));
+  var w = cross(v0, v1), l = sqrt(Versor.dot(w, w));
   if (!l) return [1, 0, 0, 0];
-  var t = alpha * acos(max(-1, min(1, dot(v0, v1)))) / 2, s = sin(t); // t = θ / 2
+  var t = alpha * acos(max(-1, min(1, Versor.dot(v0, v1)))) / 2, s = sin(t); // t = θ / 2
   return [cos(t), w[2] / l * s, -w[1] / l * s, w[0] / l * s];
 };
 
-// Returns the quaternion that represents q0 * q1.
-versor.multiply = function(q0, q1) {
-  return [
-    q0[0] * q1[0] - q0[1] * q1[1] - q0[2] * q1[2] - q0[3] * q1[3],
-    q0[0] * q1[1] + q0[1] * q1[0] + q0[2] * q1[3] - q0[3] * q1[2],
-    q0[0] * q1[2] - q0[1] * q1[3] + q0[2] * q1[0] + q0[3] * q1[1],
-    q0[0] * q1[3] + q0[1] * q1[2] - q0[2] * q1[1] + q0[3] * q1[0]
-  ];
-};
-
+// Why only 3 dimensions??? TBD
 function cross(v0, v1) {
   return [
     v0[1] * v1[2] - v0[2] * v1[1],
     v0[2] * v1[0] - v0[0] * v1[2],
-    v0[0] * v1[1] - v0[1] * v1[0]
+    v0[0] * v1[1] - v0[1] * v1[0],
+    0
   ];
-}
-
-function dot(v0, v1) {
-  return v0[0] * v1[0] + v0[1] * v1[1] + v0[2] * v1[2];
 }
 
 export default versor;
